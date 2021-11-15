662b9c1311888862af26e2511d4dc0a4658dd205
Manish Gautam <>
2021-11-15 10:27:21 +0100

    Initial Commit

---
+++ src/views/Tab1.vue
@@ -0,0 +1,51 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding" scroll-y="false">
+      <ion-slides pager="true" :options="slideOpts">
+        <ion-slide>
+          <div class="slide">
+            <img src="assets/slider-1.svg" />
+            <h2>Connect easily with your family and friends over countries</h2>
+          </div>
+        </ion-slide>
+
+        <ion-slide>
+          <div class="slide">
+            <img src="assets/slider-1.svg" />
+            <h2>Connect easily with your family and friends over countries</h2>
+          </div>
+        </ion-slide>
+
+        <ion-slide>
+          <div class="slide">
+            <img src="assets/slider-1.svg" />
+            <h2>Connect easily with your family and friends over countries</h2>
+          </div>
+        </ion-slide>
+
+        <ion-slide>
+          <div class="slide">
+            <img src="assets/slider-1.svg" />
+            <h2>Connect easily with your family and friends over countries</h2>
+          </div>
+        </ion-slide>
+      </ion-slides>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonSlides, IonSlide, IonContent, IonPage } from "@ionic/vue";
+
+export default {
+  name: "Tab1",
+  components: { IonSlides, IonSlide, IonContent, IonPage },
+  setup() {
+    const slideOpts = {
+      initialSlide: 1,
+      speed: 400,
+    };
+    return { slideOpts };
+  },
+};
+</script>
---
+++ src/views/Tab2.vue
@@ -0,0 +1,58 @@
+<template>
+  <ion-page>
+    <ion-content fullscreen>
+      <ion-list>
+        <ion-list-header> Chats </ion-list-header>
+
+        <ion-item>
+          <ion-avatar slot="start">
+            <img src="https://i.pravatar.cc/300" />
+          </ion-avatar>
+          <ion-label>
+            <h2>Minsk</h2>
+            <h3>Bob says hi</h3>
+            <ion-badge class="slider-message" color="danger">2</ion-badge>
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-avatar slot="start">
+            <img src="https://i.pravatar.cc/300" />
+          </ion-avatar>
+          <ion-label>
+            <h2>Finn</h2>
+            <h3>I'm a big deal</h3>
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-avatar slot="start">
+            <img src="https://i.pravatar.cc/300" />
+          </ion-avatar>
+          <ion-label>
+            <h2>Finn</h2>
+            <h3>I'm a big deal</h3>
+          </ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonList,
+  IonLabel,
+  IonItem,
+  IonListHeader,
+  IonAvatar,
+  IonPage,
+  IonContent,
+  IonBadge
+} from "@ionic/vue";
+
+export default {
+  name: "Tab2",
+  components: { IonList, IonLabel, IonItem, IonListHeader, IonAvatar, IonContent, IonPage, IonBadge},
+};
+</script>
