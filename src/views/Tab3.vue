b4a1732b562643aaed7622c170329de6539fd7ab
Manish Gautam <>
2021-11-15 17:10:22 +0100

    Profile page display

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -1,41 +1,33 @@
 <template>
+<ion-header> 
+    <ion-toolbar>
+      <ion-title>My Profile</ion-title>
+    </ion-toolbar>
+</ion-header>
   <ion-page>
+
     <ion-content fullscreen>
-      <ion-list>
-        <ion-list-header> My profile </ion-list-header>
-
-        <ion-grid>
-          <ion-row>
-            <ion-col> </ion-col>
-
-            <ion-col>
-              <ion-avatar class="avt" slot="start">
-                <img src="https://i.pravatar.cc/300" />
-              </ion-avatar>
-            </ion-col>
-
-            <ion-col> </ion-col>
-          </ion-row>
-        </ion-grid>
-      </ion-list>
+      <ion-grid class="h-100">
+        <ion-row class="h-100">
+          <ion-col class="text-center">
+            <div class="ion-margin-bottom">
+              <img class="profile-picture" src="https://i.pravatar.cc/300" />
+            </div>
+            <ion-text class="secondary-clr"> id5 </ion-text>
+            <ion-text> <h1>Helen Jobs</h1> </ion-text>
+            <ion-text class="secondary-clr"> 19 yr </ion-text>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import {
-  IonList,
-  IonListHeader,
-  IonAvatar,
-  IonPage,
-  IonContent,
-  IonGrid,
-  IonRow,
-  IonCol
-} from "@ionic/vue";
+import { IonPage, IonContent, IonGrid, IonRow, IonCol } from "@ionic/vue";
 
 export default {
   name: "Tab3",
-  components: { IonList, IonListHeader, IonAvatar, IonContent, IonPage, IonGrid, IonRow, IonCol },
+  components: { IonContent, IonPage, IonGrid, IonRow, IonCol },
 };
 </script>
